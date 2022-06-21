de7d7a9ee822dd5f0bfef81fa6dc949d6a5f1971
David Vargas <dvargas92495@gmail.com>
2022-06-21 13:07:01 -0400

    Several landing page improvements

---
+++ app/data/submitToolRequest.server.ts
@@ -0,0 +1,24 @@
+import sendEmail from "aws-sdk-plus/dist/sendEmail";
+
+const submitToolRequest = ({
+  email,
+  tool,
+  message,
+}: {
+  email: string;
+  tool: string;
+  message: string;
+}): Promise<{ success: boolean }> => {
+  return sendEmail({
+    to: "support@samepage.network",
+    from: "support@samepage.network",
+    body: `User has requested ${tool} be added to the SamePage Network.\n\n${message}`,
+    subject: "New tool request for SamePage",
+    replyTo: email,
+  }).then(() => ({
+    success: true,
+    message: "Successfully submitted tool request!",
+  }));
+};
+
+export default submitToolRequest;
