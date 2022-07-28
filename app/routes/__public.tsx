b48129a98fc9e5fa1f39cda77017974d6b0ff9dd
David Vargas <dvargas92495@gmail.com>
2022-07-28 19:53:41 -0400

    bring website back online

--- app/routes/__public.tsx
+++ app/routes/__public.tsx
@@ -1,3 +1,13 @@
 import PublicLayout from "@dvargas92495/app/components/PublicLayout";
-const Public: React.FC = () => <PublicLayout isWaitlist />;
+import type { LoaderFunction } from "@remix-run/node";
+import getUserId from "@dvargas92495/app/backend/getUserId.server";
+
+export { default as CatchBoundary } from "@dvargas92495/app/components/DefaultCatchBoundary";
+export { default as ErrorBoundary } from "@dvargas92495/app/components/DefaultErrorBoundary";
+
+const Public: React.FC = () => <PublicLayout isWaitlist pages={["docs"]} />;
+
+export const loader: LoaderFunction = ({ request }) => {
+  return getUserId(request).then((id) => !!id);
+};
 export default Public;
---
+++ app/routes/__public/docs/$.tsx
@@ -0,0 +1,5 @@
+const DocsPage = (): React.ReactElement => {
+  return <div></div>;
+};
+
+export default DocsPage;
