1e2fa1186c5946e8e5d5a008540506e6c6707cbb
David Vargas <dvargas92495@gmail.com>
2022-08-11 16:32:54 -0400

    trying out consistent versions of remix to fix hydration

--- app/routes/__private/user.tsx
+++ app/routes/__private/user.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import getMeta from "@dvargas92495/app/utils/getMeta";
 import UserDashboard from "@dvargas92495/app/components/UserDashboard";
+// import Button from "@dvargas92495/app/components/Button";
 
 const TABS = ["page", "tab", "hello"];
 
-
 const UserPage: React.FunctionComponent = () => {
-  return <UserDashboard tabs={TABS} title={"Samepage"} />;
+  return (
+    <div className="relative h-full">
+      <UserDashboard tabs={TABS} title={"Samepage"} />
+      {/* <Button className={"absolute right-1 bottom-1"}>Log out</Button> */}
+    </div>
+  );
 };
 
 export const meta = getMeta({
