08ec56395fa0c7c2866a3bb404b3d81119734b15
David Vargas <dvargas92495@gmail.com>
2022-08-11 12:15:24 -0400

    GITHUB_PERSONAL provider

--- data/main.ts
+++ data/main.ts
@@ -1,6 +1,6 @@
 import base from "fuegojs/dist/base";
 import schema from "./schema";
-import { ActionsSecret } from "@cdktf/provider-github";
+import { ActionsSecret, GithubProvider } from "@cdktf/provider-github";
 
 base({
   projectName: "samepage.network",
@@ -15,15 +15,22 @@ base({
     const accessSecret = this.node.children.find(
       (c) => c.node.id === "deploy_aws_access_key"
     );
+
+    const provider = new GithubProvider(this, "GITHUB_PERSONAL", {
+      token: process.env.GITHUB_TOKEN,
+      owner: "dvargas92495",
+    });
     new ActionsSecret(this, "logseq_deploy_aws_access_key", {
       repository: "logseq-samepage",
       secretName: "DEPLOY_AWS_ACCESS_KEY",
       plaintextValue: (accessKey as ActionsSecret).plaintextValue,
+      provider,
     });
     new ActionsSecret(this, "logseq_deploy_aws_access_secret", {
       repository: "logseq-samepage",
       secretName: "DEPLOY_AWS_ACCESS_SECRET",
       plaintextValue: (accessSecret as ActionsSecret).plaintextValue,
+      provider,
     });
   },
 });
