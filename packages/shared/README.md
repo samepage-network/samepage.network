19a94877e354a2d418b7fd472af55ad9d8c3e7da
David Vargas <dvargas92495@gmail.com>
2022-08-17 16:27:58 -0400

    0.1.0 of the shared package

---
+++ packages/shared/README.md
@@ -0,0 +1,5 @@
+# @samepage/shared
+
+Utilities and standards that are used in both the front end packages and the backend network.
+
+For more documentation, visit [our developer docs](https://samepage.network/docs/developer).
---
+++ packages/shared/src/apps.ts
@@ -0,0 +1,22 @@
+const APPS = [
+  {
+    id: 1,
+    name: "Roam",
+    key: "roamjs",
+  },
+  {
+    id: 2,
+    name: "LogSeq",
+  },
+  {
+    id: 3,
+    name: "Obsidian",
+  },
+] as const;
+
+export type App = typeof APPS[number]
+export type AppId = App["id"];
+
+export const appNameById = Object.fromEntries(APPS.map((a) => [a.id, a.name]));
+
+export default APPS;
