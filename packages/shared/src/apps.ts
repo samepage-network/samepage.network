8d9283d8a3f4473abd1b1d933f324cece2637c3d
David Vargas <dvargas92495@gmail.com>
2022-08-31 13:09:50 -0400

    add 0 as a valid app id type

--- packages/shared/src/apps.ts
+++ packages/shared/src/apps.ts
@@ -15,6 +15,14 @@ export const APPS = [
 ] as const;
 
 export type App = typeof APPS[number];
-export type AppId = App["id"];
+
+// Playing around with the idea of the SamePage Network as App 0
+// In the future, we will want organizations to be able to self host networks
+// on whichever cloud or on-prem solution they want. These networks should have global
+// ids and labels just like apps, making it all addressable via the 0 app: 
+// - SamePage/Main
+// - SamePage/Org
+// - etc.
+export type AppId = App["id"] | 0;
 
 export const appNameById = Object.fromEntries(APPS.map((a) => [a.id, a.name]));
