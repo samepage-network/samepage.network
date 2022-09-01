88dc89e73f8d35d8a3b279f86b4dddbb3430bae2
David Vargas <dvargas92495@gmail.com>
2022-09-01 13:32:17 -0400

    fix automerge import

--- app/routes/__private/admin/components/notification-container.tsx
+++ app/routes/__private/admin/components/notification-container.tsx
@@ -1,26 +1,26 @@
 import Button from "@dvargas92495/app/components/Button";
 import TextInput from "@dvargas92495/app/components/TextInput";
-import NotificationContainer, {
-  notify,
-} from "client/src/components/NotificationContainer";
+import NotificationContainer from "client/src/components/NotificationContainer";
+import dispatchAppEvent from "client/src/internal/dispatchAppEvent";
 
 const NotificationContainerPage = () => {
   return (
     <>
       <div className={"mb-8"}>
-        <TextInput name={"title"} placeholder={"Title"} />
-        <TextInput name={"description"} placeholder={"Description"} />
+        <TextInput name={"pageUuid"} placeholder={"Global Id"} />
+        <TextInput name={"notebookPageId"} placeholder={"Local Id"} />
         <Button
           onClick={() =>
-            notify({
-              title:
-                document.querySelector<HTMLInputElement>("input[name=title]")
-                  ?.value || "None",
-              description:
+            dispatchAppEvent({
+              type: "share-page",
+              notebookPageId:
                 document.querySelector<HTMLInputElement>(
-                  "input[name=description]"
+                  "input[name=notebookPageId]"
                 )?.value || "None",
-              buttons: ["accept"],
+              pageUuid:
+                document.querySelector<HTMLInputElement>("input[name=pageUuid]")
+                  ?.value || "None",
+              source: { app: 0, workspace: "main" },
             })
           }
         >
