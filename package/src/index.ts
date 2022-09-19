ac80195a42805f3cd110797e7f199b2711bf0fc5
David Vargas <dvargas92495@gmail.com>
2022-09-19 13:49:55 -0400

    fixing some build issues with package cli

--- package/src/index.ts
+++ package/src/index.ts
@@ -1,5 +1,7 @@
 throw new Error(
-  `Do not import from @samepage/client directly. Instead import the *method* you need directly. For example: 
+  `Do not import from samepage directly. Instead import the *method* you need directly. For example: 
     
-import setupSamePageClient from "@samepage/client/protocols/setupSamePageClient;"`
+import setupSamePageClient from "samepage/protocols/setupSamePageClient;"`
 );
+
+export {};
--- package/src/scripts/cli.ts
+++ package/src/scripts/cli.ts
@@ -52,7 +52,7 @@ const run = async (command: string, args: string[]): Promise<number> => {
     case "test":
       return test(opts);
     case "publish":
-      return publish(opts);
+      return publish();
     default:
       console.error("Command", command, "is unsupported");
       return 1;
