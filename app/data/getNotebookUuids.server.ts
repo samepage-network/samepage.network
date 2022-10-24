7a39cf0009057ed309eca08fb05486b4cb1f2d14
David Vargas <dvargas92495@gmail.com>
2022-10-24 11:11:05 -0400

    Notfound error for get notebook uuids

--- app/data/getNotebookUuids.server.ts
+++ app/data/getNotebookUuids.server.ts
@@ -1,5 +1,7 @@
 import type { Notebook } from "package/internal/types";
 import getMysql from "fuegojs/utils/mysql";
+import { NotFoundError } from "@dvargas92495/app/backend/errors.server";
+import { appsById } from "package/internal/apps";
 
 const getNotebookUuids = ({
   workspace,
@@ -17,7 +19,9 @@ const getNotebookUuids = ({
       .then(([results]) => {
         const links = results as { uuid: string }[];
         if (!links.length) {
-          throw new Error("Could not find notebook uuid");
+          throw new NotFoundError(
+            `Could not find notebook with workspace name: ${workspace} in app ${appsById[app].name}`
+          );
         }
         return links.map((l) => l.uuid);
       })
