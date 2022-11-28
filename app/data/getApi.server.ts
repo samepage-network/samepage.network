9d4722f8af1779aecc2747d8db23670e22efd5d1
David Vargas <dvargas92495@gmail.com>
2022-11-28 01:21:09 -0500

    api yaml detects app/data, remove experimental apply flag

--- app/data/getApi.server.ts
+++
@@ -1,8 +0,0 @@
-import { ApiGatewayManagementApi } from "@aws-sdk/client-apigatewaymanagementapi";
-
-const getApi = (): ApiGatewayManagementApi =>
-  new ApiGatewayManagementApi({
-    endpoint: `https://${process.env.API_GATEWAY_ID}.execute-api.us-east-1.amazonaws.com/production`,
-  });
-
-export default getApi;
--- app/data/postToConnection.server.ts
+++ app/data/postToConnection.server.ts
@@ -1,6 +1,11 @@
 import { v4 } from "uuid";
 import axios from "axios";
-import getApi from "./getApi.server";
+import { ApiGatewayManagementApi } from "@aws-sdk/client-apigatewaymanagementapi";
+
+const getApi = (): ApiGatewayManagementApi =>
+  new ApiGatewayManagementApi({
+    endpoint: `https://${process.env.API_GATEWAY_ID}.execute-api.us-east-1.amazonaws.com/production`,
+  });
 
 type SendData = {
   ConnectionId: string;
