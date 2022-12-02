5d8b9e2e64dc23f9268e5ffbaaa450b0abe8334e
David Vargas <dvargas92495@gmail.com>
2022-12-01 23:40:50 -0500

    pass in original annotation index to the render json parts

--- package/utils/renderAtJson.ts
+++ package/utils/renderAtJson.ts
@@ -16,11 +16,12 @@ const renderAtJson = ({
   applyAnnotation: {
     [t in Annotation as t["type"]]?:
       | AppliedAnnotation
-      | ((
-          attributes: t["attributes"],
-          content: string,
-          appAttributes: Record<string, string>
-        ) => AppliedAnnotation);
+      | ((args: {
+          attributes: t["attributes"];
+          content: string;
+          appAttributes: Record<string, string>;
+          index: number;
+        }) => AppliedAnnotation);
   };
 }) => {
   return state.annotations
@@ -30,38 +31,49 @@ const renderAtJson = ({
       const bsize = b.annotation.end - b.annotation.start;
       return bsize - asize || a.index - b.index;
     })
-    .map(({ annotation }) => annotation)
     .reduce((p, c, index, all) => {
-      const appliedAnnotationData = applyAnnotation[c.type];
-      const annotatedContent = p.slice(c.start, c.end);
+      const appliedAnnotationData = applyAnnotation[c.annotation.type];
+      const annotatedContent = p.slice(c.annotation.start, c.annotation.end);
       const appliedAnnotation =
         typeof appliedAnnotationData === "object"
           ? appliedAnnotationData
           : typeof appliedAnnotationData === "function"
-          ? appliedAnnotationData(
+          ? appliedAnnotationData({
               // @ts-ignore
-              c.attributes || {},
-              annotatedContent,
-              c.appAttributes?.[appsById[app].name.toLowerCase()] || {}
-            )
+              attributes: c.annotation.attributes || {},
+              content: annotatedContent,
+              appAttributes:
+                c.annotation.appAttributes?.[
+                  appsById[app].name.toLowerCase()
+                ] || {},
+              index: c.index,
+            })
           : { prefix: "", suffix: "" };
       all.slice(index + 1).forEach((a) => {
-        a.start +=
-          (a.start >= c.start ? appliedAnnotation.prefix.length : 0) +
-          (a.start >= c.end ? appliedAnnotation.suffix.length : 0) +
-          (appliedAnnotation.replace && a.start >= c.end
+        a.annotation.start +=
+          (a.annotation.start >= c.annotation.start
+            ? appliedAnnotation.prefix.length
+            : 0) +
+          (a.annotation.start >= c.annotation.end
+            ? appliedAnnotation.suffix.length
+            : 0) +
+          (appliedAnnotation.replace && a.annotation.start >= c.annotation.end
             ? -annotatedContent.length
             : 0);
-        a.end +=
-          (a.end >= c.start ? appliedAnnotation.prefix.length : 0) +
-          (a.end > c.end ? appliedAnnotation.suffix.length : 0) +
-          (appliedAnnotation.replace && a.end > c.end
+        a.annotation.end +=
+          (a.annotation.end >= c.annotation.start
+            ? appliedAnnotation.prefix.length
+            : 0) +
+          (a.annotation.end > c.annotation.end
+            ? appliedAnnotation.suffix.length
+            : 0) +
+          (appliedAnnotation.replace && a.annotation.end > c.annotation.end
             ? -annotatedContent.length
             : 0);
       });
-      return `${p.slice(0, c.start)}${appliedAnnotation.prefix}${
+      return `${p.slice(0, c.annotation.start)}${appliedAnnotation.prefix}${
         appliedAnnotation.replace ? "" : annotatedContent
-      }${appliedAnnotation.suffix}${p.slice(c.end)}`;
+      }${appliedAnnotation.suffix}${p.slice(c.annotation.end)}`;
     }, state.content.toString());
 };
 
