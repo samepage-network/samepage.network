2fb7796b115a8ffa2851b58fd6474c93a7e8c25c
David Vargas <dvargas92495@gmail.com>
2022-12-07 00:28:05 -0500

    remove log

--- package/utils/mergeDocs.ts
+++ package/utils/mergeDocs.ts
@@ -13,7 +13,6 @@ const mergeDocs = (
     const offset = latestDoc.content.length;
     latestDoc.content.insertAt?.(offset, ...preExistingDoc.content);
     const merged = convertAnnotations(preExistingDoc.annotations);
-    console.log(Object.keys(merged[0].startIndex));
     merged.forEach((a) => {
       a.startIndex = new Automerge.Counter(offset + a.startIndex.value);
       a.endIndex = new Automerge.Counter(offset + a.endIndex.value);
