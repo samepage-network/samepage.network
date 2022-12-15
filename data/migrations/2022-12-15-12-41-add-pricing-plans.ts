5a4db1697b6216e5c58ae6f0250b0055ec5f4494
David Vargas <dvargas92495@gmail.com>
2022-12-15 12:45:28 -0500

    migrate pricing plan quotas

---
+++ data/migrations/2022-12-15-12-41-add-pricing-plans.ts
@@ -0,0 +1,29 @@
+import type { MigrationProps } from "fuegojs/types";
+import QUOTAS from "~/data/quotas.server";
+
+export const migrate = async (args: MigrationProps) => {
+  const PAGES = QUOTAS.indexOf("Pages");
+  const NOTEBOOKS = QUOTAS.indexOf("Notebooks");
+  return args.connection.execute(
+    `INSERT INTO quotas (uuid, field, value) 
+     VALUES (UUID(), ?, ?, ?), (UUID(), ?, ?, ?), (UUID(), ?, ?, ?), (UUID(), ?, ?, ?)`,
+    [
+      PAGES,
+      1000,
+      "price_1MFLQsAwmvpmkmv4fGoEheh0",
+      NOTEBOOKS,
+      5,
+      "price_1MFLQsAwmvpmkmv4fGoEheh0",
+      PAGES,
+      10000,
+      "price_1MFLQmAwmvpmkmv4zCPlQnIf",
+      NOTEBOOKS,
+      8,
+      "price_1MFLQmAwmvpmkmv4zCPlQnIf",
+    ]
+  );
+};
+
+export const revert = () => {
+  return Promise.resolve();
+};
