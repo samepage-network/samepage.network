bee64d4805393a4caafd6d569223e1af5042894e
David Vargas <dvargas92495@gmail.com>
2022-12-18 10:44:05 -0500

    fix notebook quota

--- app/data/getQuota.server.ts
+++ app/data/getQuota.server.ts
@@ -19,11 +19,9 @@ const getQuota = async ({
   tokenUuid: string;
 }) => {
   const cxn = await getMysql(requestId);
-  console.log("globalContext", JSON.stringify(globalContext));
   const userId = await cxn
     .execute(`SELECT user_id FROM tokens WHERE uuid = ?`, [tokenUuid])
     .then(([r]) => (r as { user_id: string }[])[0]?.user_id);
-  console.log("userId", JSON.stringify(userId));
   const stripeId = userId
     ? await users
         .getUser(userId)
@@ -31,21 +29,22 @@ const getQuota = async ({
           const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || "", {
             apiVersion: "2022-11-15",
           });
-          return stripe.subscriptions.list({
-            customer: u.privateMetadata.stripeCustomerId as string,
-          });
+          const customer = u.privateMetadata.stripeCustomerId as string;
+          return customer
+            ? stripe.subscriptions
+                .list({
+                  customer,
+                })
+                .then((subs) => subs.data[0].items.data[0].price.id)
+            : "";
         })
-        .then((subs) => subs.data[0].items.data[0].price.id)
         .catch((e) => {
           console.error(e);
           return "";
         })
     : "";
-  console.log("stripeId", JSON.stringify(stripeId));
   const quotasInThisPlan = globalContext.quotas[stripeId];
-  console.log("quotasInThisPlan", JSON.stringify(quotasInThisPlan));
   const storedValue = quotasInThisPlan?.[field];
-  console.log("storedValue", JSON.stringify(storedValue || "NULL"));
   if (typeof storedValue !== "undefined") return storedValue;
   return cxn
     .execute(
