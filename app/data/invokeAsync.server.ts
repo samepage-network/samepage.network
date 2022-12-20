cb65297090a1a8aaec3afd5e830a8d63ba035a92
David Vargas <dvargas92495@gmail.com>
2022-12-19 23:00:17 -0500

    return to sync async

--- app/data/invokeAsync.server.ts
+++ app/data/invokeAsync.server.ts
@@ -2,8 +2,9 @@ import { Lambda } from "@aws-sdk/client-lambda";
 import { domain } from "@dvargas92495/app/backend/constants.server";
 import axios from "axios";
 import { v4 } from "uuid";
-import { spawn } from "child_process";
-import fs from "fs";
+import { handler as uploadToIpfs } from "../../api/upload-to-ipfs";
+// import { spawn } from "child_process";
+// import fs from "fs";
 
 const lambda = new Lambda({ region: process.env.AWS_REGION });
 
@@ -31,14 +32,19 @@ const invokeAsync =
         path: string;
         data: T;
       }) => {
-        const requestId = v4();
-        const dataPath = `/tmp/${requestId}.json`;
-        fs.writeFileSync(dataPath, JSON.stringify(data));
-        spawn(`ts-node`, [`api/${path}.ts`, requestId], {
-          stdio: "inherit",
-          env: process.env,
-        });
-        return true;
+        // TODO: we might actually want this done in a child process
+        return path === "upload-to-ipfs"
+          ? // @ts-ignore
+            uploadToIpfs({...data, dry: true}, { awsRequestId: v4() }).then(() => true)
+          : Promise.reject(new Error(`Unknown path: ${path}`));
+        // const requestId = v4();
+        // const dataPath = `/tmp/${requestId}.json`;
+        // fs.writeFileSync(dataPath, JSON.stringify(data));
+        // spawn(`ts-node`, [`api/${path}.ts`, requestId], {
+        //   stdio: "inherit",
+        //   env: process.env,
+        // });
+        // return true;
       }
     : <T extends Record<string, unknown>>({
         path,
