3656beb24a18ce010cd7cc40abf7e38b7ad737fd
David Vargas <dvargas92495@gmail.com>
2023-02-22 17:08:43 -0500

    another action fix attempt

--- scripts/debug.js
+++ scripts/debug.js
@@ -1,5 +1,10 @@
 const mysql = require("mysql2/promise");
+const exec = require("child_process").execSync;
 
+exec(
+  `mysql -uroot -proot -hlocalhost -P3306 -Dsamepage_network -e "SHOW GLOBAL VARIABLES LIKE 'PORT';"`
+);
+console.log("RAN THE INLINE VERSION");
 mysql
   .createConnection("mysql://root:root@localhost:3306/samepage_network")
   .then((con) => {
