7c2cdb4f336363419be613511d0805e8375b20c9
David Vargas <dvargas92495@gmail.com>
2023-04-02 22:48:37 -0400

    ignore mocks from deploying

--- scripts/commands/update.ts
+++ scripts/commands/update.ts
@@ -39,13 +39,18 @@ const BUILD_DIR = "build";
 const options = {
   date: new Date("09-24-1995"),
 };
+const ignorePaths = ["mocks"];
 const update = ({}: {}): Promise<number> => {
   // including a date in the zip produces consistent hashes
   const name = path.basename(process.cwd());
   return fs.existsSync(BUILD_DIR)
     ? Promise.all(
         readDir(BUILD_DIR)
-          .filter((f) => /\.js$/.test(f))
+          .filter(
+            (f) =>
+              /\.js$/.test(f) &&
+              !ignorePaths.some((i) => new RegExp(`^${BUILD_DIR}/${i}`).test(f))
+          )
           .map((f) => {
             const apiName = name.replace(/\./g, "-");
             const zip = archiver("zip", { gzip: true, zlib: { level: 9 } });
