a56e68e11051e206ceaa08d67d26454f8c1906f5
David Vargas <dvargas92495@gmail.com>
2023-04-26 13:44:08 -0400

    Allow origin on releases

--- app/routes/__public/releases.$owner.$repo.$file.ts
+++ app/routes/__public/releases.$owner.$repo.$file.ts
@@ -1,13 +1,30 @@
-import { LoaderFunction } from "@remix-run/node";
+import { ActionFunction, LoaderFunction } from "@remix-run/node";
 
-export const loader: LoaderFunction = ({ params }) => {
+export const action: ActionFunction = ({ request }) => {
+  console.log("action??");
+  return request.method;
+};
+
+export const loader: LoaderFunction = ({ params, request }) => {
   const { owner = "", repo = "", file = "" } = params;
   // remove this exception once all extensions are on samepage >= 0.65.0
   if (/\.md$/.test(file))
     return fetch(
-      `https://raw.githubusercontent.com/${owner}/${repo}/main/${file}`
-    );
+      `https://raw.githubusercontent.com/${owner}/${repo}/main/${file}`,
+      {
+        method: request.method,
+      }
+    ).then((r) => {
+      r.headers.set("Access-Control-Allow-Origin", "*");
+      return r;
+    });
   return fetch(
-    `https://github.com/${owner}/${repo}/releases/latest/download/${file}`
-  );
+    `https://github.com/${owner}/${repo}/releases/latest/download/${file}`,
+    {
+      method: request.method,
+    }
+  ).then((r) => {
+    r.headers.set("Access-Control-Allow-Origin", "*");
+    return r;
+  });
 };
