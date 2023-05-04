c92473f3d8a4458adb1674a4bdf86e06d87f44fc
David Vargas <dvargas92495@gmail.com>
2023-05-04 00:33:08 -0400

    loading assets from branch (Closes #66)

---
+++ app/routes/__public/branch.$owner.$repo.$branch.$file.ts
@@ -0,0 +1,56 @@
+import { LoaderFunction } from "@remix-run/node";
+import { Octokit } from "@octokit/rest";
+import { NotFoundResponse } from "~/data/responses.server";
+import mimeTypes from "mime-types";
+import parseRemixContext from "~/data/parseRemixContext.server";
+import decompress from "decompress";
+import fs from "fs";
+
+export const loader: LoaderFunction = async ({ params, request, context }) => {
+  // TODO - actually query github for HEAD reqs
+  if (request.method === "HEAD") return new Response("", { status: 200 });
+  const { owner = "", repo = "", file = "", branch = "" } = params;
+  const octokit = new Octokit({
+    auth: process.env.GITHUB_TOKEN,
+  });
+  const {
+    data: { workflow_runs },
+  } = await octokit.actions.listWorkflowRunsForRepo({
+    owner,
+    repo,
+    branch,
+    status: "success",
+  });
+  if (!workflow_runs.length)
+    throw new NotFoundResponse(`No workflow run found`);
+  const run_id = workflow_runs[0].id;
+  const {
+    data: { artifacts },
+  } = await octokit.actions.listWorkflowRunArtifacts({
+    owner,
+    repo,
+    run_id,
+  });
+  const { url } = await octokit.actions.downloadArtifact({
+    owner,
+    repo,
+    artifact_id: artifacts[0].id,
+    archive_format: "zip",
+  });
+//   console.log("url", url);
+  const zip = await fetch(url).then((r) => r.text());
+//   console.log("zip", zip.length);
+  const tmp = `/tmp/${parseRemixContext(context).lambdaContext.awsRequestId}`;
+  fs.mkdirSync(tmp);
+  fs.writeFileSync(`${tmp}/archive.zip`, zip);
+//   const files = 
+  await decompress(`${tmp}/archive.zip`, `${tmp}/out`);
+//   console.log("depressed", files);
+  const body = fs.readFileSync(`${tmp}/out/${file}`);
+  return new Response(body, {
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Content-Type": mimeTypes.lookup(file) || "application/octet-stream",
+    },
+  });
+};
--- app/routes/__public/releases.$owner.$repo.$file.ts
+++ app/routes/__public/releases.$owner.$repo.$file.ts
@@ -1,23 +1,7 @@
-import { ActionFunction, LoaderFunction } from "@remix-run/node";
-
-export const action: ActionFunction = ({ request }) => {
-  console.log("action??");
-  return request.method;
-};
+import { LoaderFunction } from "@remix-run/node";
 
 export const loader: LoaderFunction = ({ params, request }) => {
   const { owner = "", repo = "", file = "" } = params;
-  // remove this exception once all extensions are on samepage >= 0.65.0
-  if (/\.md$/.test(file))
-    return fetch(
-      `https://raw.githubusercontent.com/${owner}/${repo}/main/${file}`,
-      {
-        method: request.method,
-      }
-    ).then((r) => {
-      r.headers.set("Access-Control-Allow-Origin", "*");
-      return r;
-    });
   return fetch(
     `https://github.com/${owner}/${repo}/releases/latest/download/${file}`,
     {
