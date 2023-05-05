c034a8dde160201d2e60b9b0a5f2704a4a107c24
David Vargas <dvargas92495@gmail.com>
2023-05-05 10:16:49 -0400

    Ok I think asset loading now works

--- app/routes/__public/assets/$app/$asset.tsx
+++ app/routes/__public/assets/$app/$asset.tsx
@@ -1,11 +1,10 @@
 import { LoaderFunction } from "@remix-run/node";
 import { Octokit } from "@octokit/rest";
+import { NotFoundResponse } from "~/data/responses.server";
+import mimeTypes from "mime-types";
 
 // TODO - inline remix-lambda-adapter and move this logic there.
-const illegalHeaders = [
-  "transfer-encoding",
-  "connection",
-];
+// const illegalHeaders = ["transfer-encoding", "connection"];
 
 export const loader: LoaderFunction = async ({ params }) => {
   const { app, asset = "" } = params;
@@ -17,8 +16,13 @@ export const loader: LoaderFunction = async ({ params }) => {
     repo: `${app}-samepage`,
     path: `assets/${asset}`,
   });
-  return fetch(content.url).then((r) => {
-    illegalHeaders.forEach((h) => r.headers.delete(h));
-    return r;
+  if (!("type" in content.data) || content.data.type !== "file") {
+    throw new NotFoundResponse(`Asset ${asset} not a file in ${app}`);
+  }
+  return new Response(Buffer.from(content.data.content, "base64"), {
+    status: 200,
+    headers: {
+      "content-type": mimeTypes.lookup(asset) || "application/octet-stream",
+    },
   });
 };
