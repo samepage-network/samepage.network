5096e59fd50a7c1a4d910301ee94cd20f677f183
David Vargas <dvargas92495@gmail.com>
2023-05-05 12:42:50 -0400

    remove bad headers once again

--- app/routes/__public/assets/$app/$asset.tsx
+++ app/routes/__public/assets/$app/$asset.tsx
@@ -4,11 +4,14 @@ import { LoaderFunction } from "@remix-run/node";
 // import mimeTypes from "mime-types";
 
 // TODO - inline remix-lambda-adapter and move this logic there.
-// const illegalHeaders = ["transfer-encoding", "connection"];
+const illegalHeaders = ["transfer-encoding", "connection"];
 
 export const loader: LoaderFunction = async ({ params }) => {
   const { app, asset = "" } = params;
   return fetch(
     `https://raw.githubusercontent.com/samepage-network/${app}-samepage/main/assets/${asset}`
-  );
+  ).then((r) => {
+    illegalHeaders.forEach((h) => r.headers.delete(h));
+    return r;
+  });
 };
