8a94656293e571644050d8d1484c5590408958fc
David Vargas <dvargas92495@gmail.com>
2023-05-18 17:08:08 -0400

    Fix fromCharCode callstack error in binary to base64

--- package/internal/binaryToBase64.ts
+++ package/internal/binaryToBase64.ts
@@ -1,7 +1,11 @@
 const binaryToBase64 = (state: Uint8Array) => {
   return typeof window === "undefined"
     ? Buffer.from(state).toString("base64")
-    : window.btoa(String.fromCharCode.apply(null, Array.from(state)));
+    : window.btoa(
+        Array.from(state)
+          .map((s) => String.fromCharCode(s))
+          .join("")
+      );
 };
 
 export default binaryToBase64;
