b930ee412f00849936a3d4b4d6d45aa095db188c
David Vargas <dvargas92495@gmail.com>
2023-12-23 13:04:34 -0500

    patch type:module

--- scripts/commands/plan.ts
+++ scripts/commands/plan.ts
@@ -2,6 +2,15 @@ import child_process from "child_process";
 import compareSqlSchemas from "../../data/compareSqlSchemas";
 import fs from "fs";
 
+const patchConfigFile = (
+  filename: string,
+  edit: (config: Record<string, unknown>) => void
+) => {
+  const config = JSON.parse(fs.readFileSync(`${filename}.json`).toString());
+  edit(config);
+  fs.writeFileSync(`${filename}.json`, JSON.stringify(config, null, 2));
+};
+
 const plan = async ({
   sql,
   tf,
@@ -12,10 +21,17 @@ const plan = async ({
   if (sql) {
     await compareSqlSchemas();
   } else {
+    // TODO - when this issue resolves, use ts-node-esm again in cdktf.json
+    // https://github.com/TypeStrong/ts-node/issues/2094
+    patchConfigFile("package", (config) => {
+      config.type = "module";
+    });
+
     // TODO - hack to get plan working in CI - look into madge warnings to see why this is happening
-    const tsconfig = JSON.parse(fs.readFileSync("tsconfig.json").toString());
-    delete tsconfig["ts-node"];
-    fs.writeFileSync("tsconfig.json", JSON.stringify(tsconfig, null, 2));
+    patchConfigFile("tsconfig", (config) => {
+      delete config["ts-node"];
+    });
+
     // TODO - make this a non speculative plan
     child_process.execSync(`npx cdktf plan`, {
       stdio: "inherit",
@@ -26,8 +42,6 @@ const plan = async ({
           }
         : { ...process.env },
     });
-    // TODO - when this issue resolves, use ts-node-esm again in cdktf.json
-    // https://github.com/TypeStrong/ts-node/issues/2094
   }
   return 0;
 };
