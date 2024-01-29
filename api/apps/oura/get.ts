ea92df84193722e591a342d314760c06136c754b
Michael Gartner <mclicks@gmail.com>
2024-01-28 19:22:16 -0600

    Update error, remove single date

--- api/apps/oura/get.ts
+++ api/apps/oura/get.ts
@@ -9,22 +9,22 @@ type ParamProps = {
   endDate: string;
   token: string;
 };
-const logic = async ({
-  dataType,
-  date, // TODO - need more data
-  endDate,
-  startDate,
-  token,
-}: ParamProps) => {
+const logic = async ({ dataType, endDate, startDate, token }: ParamProps) => {
   const url = `https://api.ouraring.com/v2/usercollection/${dataType}?start_date=${startDate}&end_date=${endDate}`;
   const headers = { Authorization: `Bearer ${token}` };
 
   try {
     const response = await fetch(url, { method: "GET", headers });
-    const data = await response.json();
-    return { data };
-  } catch (error) {
-    return { error };
+    if (!response.ok) {
+      const errorData = await response.json();
+      return { error: `API Error: ${errorData.message}` };
+    }
+    return await response.json();
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      return { error: `${error.name}: ${error.message}` };
+    }
+    return { error: "An unknown error occurred" };
   }
 };
 
