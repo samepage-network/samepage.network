edf4be509c97440cc3e9ea2d920d607fe0e20666
Michael Gartner <mclicks@gmail.com>
2024-05-10 11:15:32 -0600

    add error handling

--- api/access-token/post.ts
+++ api/access-token/post.ts
@@ -7,6 +7,7 @@ import { z } from "zod";
 import { UnauthorizedError } from "~/data/errors.server";
 import { MySql2Database } from "drizzle-orm/mysql2";
 import { MySqlColumn } from "drizzle-orm/mysql-core";
+import ServerError from "package/backend/ServerError";
 
 const bodySchema = z.object({
   code: z.string().optional(),
@@ -40,21 +41,46 @@ const fetchAccessToken = async (
   return accessTokenByCode[0]?.accessToken;
 };
 
+const maskString = (s = "") =>
+  `${s
+    .split("")
+    .slice(0, -4)
+    .map(() => "*")
+    .join("")}${s.slice(-4)}`;
+
 const logic = async (args: BackendRequest<typeof bodySchema>) => {
   const cxn = await getMysql(args.requestId);
 
   let returnedAccessToken;
-  if (args?.code) {
-    returnedAccessToken = await fetchAccessToken(
-      cxn,
-      accessTokens.code,
-      args.code
-    );
-  } else if (args?.state) {
-    returnedAccessToken = await fetchAccessToken(
-      cxn,
-      accessTokens.state,
-      args.state
+  try {
+    if (args?.code) {
+      returnedAccessToken = await fetchAccessToken(
+        cxn,
+        accessTokens.code,
+        args.code
+      );
+    } else if (args?.state) {
+      returnedAccessToken = await fetchAccessToken(
+        cxn,
+        accessTokens.state,
+        args.state
+      );
+    }
+  } catch (e) {
+    const error = e as Error;
+    await cxn.end();
+    console.error("Failed to fetch access token");
+    console.error(e);
+    console.error("Arguments:");
+    const maskedArgs = {
+      ...args,
+      code: maskString(args.code),
+      state: maskString(args.state),
+    };
+    console.error(maskedArgs);
+    throw new ServerError(
+      `Failed to get installation details: ${error.message}`,
+      401
     );
   }
 
