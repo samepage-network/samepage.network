5c820df61870b805cd6ea6b19ba1bfcb37f0db52
Michael Gartner <mclicks@gmail.com>
2024-06-28 14:24:52 -0600

    feedback

--- package/internal/unbase64.ts
+++ package/internal/unbase64.ts
@@ -1,11 +1,10 @@
-const unbase64 = (() => {
-  if (typeof window !== "undefined" && typeof window.atob === "function") {
-    return (s: string) => window.atob(s);
-  } else if (typeof Buffer !== "undefined") {
-    return (s: string) => Buffer.from(s, "base64").toString("binary");
-  } else {
-    throw new Error("No base64 decoding function available");
-  }
-})();
+const unbase64 =
+  typeof window !== "undefined" && typeof window.atob === "function"
+    ? (s: string) => window.atob(s)
+    : typeof Buffer !== "undefined"
+    ? (s: string) => Buffer.from(s, "base64").toString("binary")
+    : () => {
+        throw new Error("No base64 decoding function available");
+      };
 
 export default unbase64;
